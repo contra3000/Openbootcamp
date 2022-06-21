0e8942982d4bc4e95d7fd67fd5ce557d96c2c7b1
Juan <105739344+contra3000@users.noreply.github.com>
2022-06-21 11:57:17 -0300

    Add files via upload

---
+++ Java/Ejercicios/src/Tema2/Ejercicio1/Main.java
@@ -0,0 +1,38 @@
+package Tema2.Ejercicio1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //1.numericos
+        //1.1.enteros
+        byte variable1 = 4;
+        short variable2 = 10;
+        int variable3 = 30;
+        long variable4 = 1023949123;
+        //1.2.decimales
+        float variable5 = 5.5f;
+        double variable6 = 5.56d;
+        //2.booleanos
+        boolean variable7 = true;
+        boolean variable8 = false;
+        //3.texto
+        char variable9 = 'a';
+        String variable10 = "Juan";
+
+        System.out.println("--Numericos--");
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+        System.out.println("--Booleanos--");
+        System.out.println(variable7);
+        System.out.println(variable8);
+        System.out.println("--Texto--");
+        System.out.println(variable9);
+        System.out.println(variable10);
+
+    }
+}
